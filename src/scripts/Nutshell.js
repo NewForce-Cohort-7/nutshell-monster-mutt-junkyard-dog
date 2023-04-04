2673ac8e9ccee0561037e16ecda6cff404d25e8b
Bobby Hughes <bobbyhughes6316@gmail.com>
2023-04-04 09:47:15 -0400

    first articles ticket finished

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,6 +1,15 @@
+import { articleForm, articleList } from "./articles.js"
+
 
 export const Nutshell = () => {
+      return `
+      <div class="title-container">
+      <h1 class="articleHead">Articles</h1>
+      </div>
+      ${articleForm()}
+      ${articleList()}
 
-  
-      // Render all your UI components here
+      
+      `
 }
+
